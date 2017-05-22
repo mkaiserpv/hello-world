b73132c68c8c0b32f1817426e049a7f779cc99a3
Martha Marie Kaiser <mkaiser@pvschools.net>
2017-05-22 14:49:20 +0000

    practice with python

---
+++ practice.py
@@ -0,0 +1,25 @@
+
+my_name = "Martha Kaiser"
+print ("My name is " + my_name + ".")
+
+my_studentID = 2061234
+print ("My student ID is " + str(my_studentID) + ".")
+
+
+"""
+//studentID_sum = my_studentID[0] + my_studentID[1]
+//print ("My student ID sum is ")
+//print (studentID_sum)
+"""
+
+print (my_name[0])
+print (my_name[1])
+
+print ("My name capitalized is " + my_name.upper() + ".")
+
+
+myVacationPlans = ["sleep", "travel", "work"];
+print ("This summer I plan to " + myVacationPlans[0] + ", "
+    + myVacationPlans[1] + ", " + "and " + myVacationPlans[2] + ".")
+    
+    
